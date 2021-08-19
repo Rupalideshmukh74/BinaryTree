4b7d802726c352bb1f88ffb5851956d58ad177df
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-19 15:56:11 +0530

    [Rupali]Feat:Added:created BST and Add Element

---
+++ Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class Node<T> where T : IComparable
+    {
+        public int NodeData;
+        public Node<T> LeftTree;
+        public Node<T> RightTree;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree program!");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>();
+
+            //Display Root 
+            Console.WriteLine("Root : 56 ");
+
+            //Add Elements in Tree
+            binarySearch.Insert(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+
+            //Display Result
+            Console.WriteLine("Binary Search Tree Elements ::");
+            binarySearch.Traverse(binarySearch.root);
         }
     }
 }
